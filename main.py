f71245f4839c3192875bbab934a3ef03ca43873f
Saksham-Singh18 <thakursakshamsingh3@gmail.com>
2025-09-08 01:36:03 +0530

    create Procfile

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ login_manager.init_app(app)
 
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
